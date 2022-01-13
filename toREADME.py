62f380fd8ea1c6ddeab4001823097b60f8f1007c
caofangyu <caofangyu@fabu.ai>
2022-01-13 13:17:36 +0800

    change filename

---
+++ toREADME.py
@@ -0,0 +1,17 @@
+import os
+import sys
+headpath = "/onboard_data/bags/meishangang"
+lst = os.listdir(headpath)
+ans = []
+for x in lst:
+    currentFile = headpath +"/" + x +"/" + sys.argv[1]
+    if os.path.exists(currentFile):
+        lstInDate = os.listdir(currentFile)
+        for y in lstInDate:
+            fullPath = currentFile +"/" + y + "/README"
+            ans += fullPath + ":\n"
+            f = open(currentFile +"/" + y + "/README","r")
+            ans += f.readlines()
+            f.close()
+for line in ans:
+    print(line,end = "")
