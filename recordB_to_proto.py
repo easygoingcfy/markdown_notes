e2c840d40ea0788265327f3d38f5001046fa1d13
caofangyu <caofangyu@fabu.ai>
2022-01-18 17:43:21 +0800

    change file name

--- recordB_to_proto.py
+++ recordB_to_proto.py
@@ -4,14 +4,17 @@ import modules_common_driving_record_proto_record_pb2
 # import google.protobuf.json_format as json_format
 from google.protobuf import text_format,json_format
 def recordB_to_proto(path):
-    record = modules_common_driving_record_proto_record_pb2.Record()
-    f = open(path,"rb")
-    text_format.Parse(f.read(), record)
+    # record = modules_common_driving_record_proto_record_pb2.Record()
+    batch_record = modules_common_driving_record_proto_record_pb2.BatchRecord()
+    record = batch_record.records.add()
+    f = open(path)
+    text_format.Parse(f.read(),record)
     f.close()
+    # print(record)
     #message to Json
-    json_string = json_format.MessageToJson(record)
-    return json_string
+    json_string = json_format.MessageToDict(batch_record)
     print("message to json done")
+    return json_string
 if __name__ == '__main__':
     path = 'recordB'
     recordB_to_proto(path)
